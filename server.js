8527b40515e79a5fdc7d93a75a1633ba3d74ff62
Rabbitlancer <slendercrow@mail.ru>
2015-06-27 16:52:47 +0300

    base

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+const PORT = 2304;
+
+function handleRequest(req,res) {
+	res.end('Hi, I am from Node');
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function (){console.log('Server started')});
